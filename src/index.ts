6f75f72c3ccb0c0417efebd6a558370dfa2feecc
Antonio Ivanovski <toteto@users.noreply.github.com>
2024-05-13 13:14:48 +0200

    update import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as auth from '@lib/auth/auth';
 export * as resolver from '@lib/state/resolver';
+export { default as VidosResolver } from '@lib/state/vidosResolver';
 export * as protocol from '@lib/types-sdk';
 export { core } from '@0xpolygonid/js-sdk';
