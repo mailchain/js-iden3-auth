156be16a3f6d627250e935214f108d8b092d2395
Antonio Ivanovski <toteto@users.noreply.github.com>
2024-05-13 13:40:08 +0200

    fix prettier

--- src/state/vidosResolver.ts
+++ src/state/vidosResolver.ts
@@ -42,7 +42,7 @@ type DidResolutionResult = {
 export default class VidosResolver implements IStateResolver {
   constructor(private readonly resolverUrl: string, private readonly apiKey: string) {}
 
-  // Note: implementation closely resembles EthStateResolver because Vidos resolver internally uses the same contract. 
+  // Note: implementation closely resembles EthStateResolver because Vidos resolver internally uses the same contract.
   // The only difference is the usage of regular HTTP requests instead of web3 calls.
 
   async rootResolve(state: bigint): Promise<ResolvedState> {
